7d672cdba2cec833159d70480634b43bfca5d836
Arthanz <nathanbara9@gmail.com>
2022-12-21 08:28:54 +0700

    Pertemuan 2

---
+++ database/migrations/2022_12_20_104635_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();// Primary Key
+            $table->string('Judul');
+            $table->timestamps();
+            $table->date('PublishDate');
+            $table->string('Author');
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
